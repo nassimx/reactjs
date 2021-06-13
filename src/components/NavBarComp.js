b8601462aaa4950eacd9605fe9af012a7d5e6963
nassimx <mr.nassimfr@gmail.com>
2021-06-13 12:08:16 +0100

    NavBar

---
+++ src/components/NavBarComp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Navbar,Nav,Container } from 'react-bootstrap'
+
+export default class NavBarComp extends Component {
+    render() {
+        return (
+            <div>
+                 <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Digital Future</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+            </div>
+        )
+    }
+}
