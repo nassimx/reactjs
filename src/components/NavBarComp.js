03c7a19dd91ee99950b9fad2b55b9c8308283a7a
nassimx <mr.nassimfr@gmail.com>
2021-06-14 09:32:55 +0100

    navbar commit

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -1,21 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
-import { Navbar,Nav,Container } from 'react-bootstrap'
+import logo from '../logo.png'
+import Navbar from 'react-bootstrap/Navbar'
+
+//* React fontawesome //
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Nav } from 'react-bootstrap';
 
 export default class NavBarComp extends Component {
     render() {
         return (
-            <div>
-                 <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Digital Future</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+           
+<Navbar bg="light" expand="sm">
+<div className="container">
+  <Navbar.Brand href="#"><img className="logo" src={logo} alt="Digital Future" /></Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="ml-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link href="#Home">Home</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Services">Services</Nav.Link>
+      <Nav.Link href="#Contact Us">Contact Us</Nav.Link>
+      
+     
     </Nav>
-    </Container>
-  </Navbar>
-            </div>
+  </Navbar.Collapse>
+  </div>
+
+</Navbar>
         )
     }
 }
